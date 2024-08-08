b7a5c6aee9dfbfe8cd07845758928f60e7b9062d
polic44 <166876853+polic44@users.noreply.github.com>
2024-08-08 16:24:41 +0200

    Trójkąt odwrócony przy pomocy gwiazdek w C++
    Trójkąt odwrócony przy pomocy gwiazdek w C++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,j;
+    for(i=10;i>0;i--) {
+        for (j = i; j >0; j--)
+        {
+
+            {
+                cout<<"*";
+            }
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
